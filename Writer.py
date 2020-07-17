eb1438f8955ff0b161fdc0478ac90d7d92df87b6
Nicolas Gauthier <ngauthier12@gmail.com>
2020-07-16 21:28:10 -0400

    rebuilding graph from json data

--- Writer.py
+++ Writer.py
@@ -1,16 +1,27 @@
 import io
 import json
 
+import Shared
 from Node import *
 
 
 class Writer:
 
     def __init__(self, depth):
-        pass
+        self.nodeByWords = {}
 
     def load(self, path):
-        pass
+        data = None
+        with io.open(path, mode="r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        for key, nodeData in data.items():
+            node = Node(key)
+            self.nodeByWords[key] = node
+
+            for word, count in nodeData.items():
+                next_node = Shared.get_or_create_node(self.nodeByWords, [word])
+                node.countByNextNode[next_node] = count
 
     def write(self, path):
         pass
